7e0247a80239bc32a955e15c6627fe7b5e605f22
Phlipr <preynolds1406@gmail.com>
2017-06-15 16:22:57 -0500

    Added Coursera Index controller to app.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'courses/index'
+
   get 'greeter/hello'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
